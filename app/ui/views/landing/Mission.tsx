1fb9b2159ecf90b80e14dbdaa60cc1f3054f7c3a
manueljenni <manuelnoahjenni@outlook.com>
2024-10-12 17:35:55 +0700

    Fix mission

--- app/ui/views/landing/Mission.tsx
+++ app/ui/views/landing/Mission.tsx
@@ -9,9 +9,9 @@ export default function Mission() {
   return (
     <div className="flex flex-col justify-center items-center bg-lightBackground pb-12 mt-18 px-6 lg:px-12 space-y-16">
       <div className="flex flex-col justify-center items-center w-full">
-        <div className="flex flex-col md:flex-row justify-between max-w-6xl w-full space-y-8 md:space-y-0 md:space-x-4">
-          <div className="space-y-4 text-center md:text-left md:max-w-xl">
-            <h1 className="text-[#232E23] text-5xl font-semibold md:leading-tight">
+        <div className="flex flex-col lg:flex-row justify-between max-w-6xl w-full space-y-8 lg:space-y-0 lg:space-x-4">
+          <div className="space-y-4 text-center lg:text-left max-w-2xl mx-auto">
+            <h1 className="text-[#232E23] text-5xl font-semibold lg:leading-tight ">
               Unsere Mission ist es, Dir zu einem gesünderen Lebensstil zu verhelfen
             </h1>
             <p className="pb-4 text-lg">
@@ -22,14 +22,14 @@ export default function Mission() {
               Jetzt Test starten
             </Button>
           </div>
-          <div className="flex justify-start items-start w-full md:w-auto overflow-x-auto">
+          <div className="flex justify-start items-start w-full lg:w-auto overflow-x-auto">
             <div className="relative w-full z-50">
-              <div className="flex md:flex-col flex-row gap-4 items-center justify-start overflow-x-auto no-scrollbar w-full z-50 h-full py-2">
+              <div className="flex lg:flex-col flex-row gap-4 items-center justify-start overflow-x-auto no-scrollbar w-full z-50 h-full py-2 px-1">
                 <Ingredient
                   image={vitaminC}
                   title="2x Vitamin C"
                   text="Der Klassiker für das Immunsystem und die Kollagenbildung."
-                  className="md:-rotate-2"
+                  className="lg:-rotate-2"
                 />
                 <Ingredient
                   image={omega3}
@@ -40,7 +40,7 @@ export default function Mission() {
                   image={magnesium}
                   title="1x Tri-Magnesiumdicitrat"
                   text="Der Elektrolyt für die Muskelfunktion."
-                  className="md:rotate-2"
+                  className="lg:rotate-2"
                 />
               </div>
             </div>
