f64e04111059a6880f6c5ce9c290924989c413a0
manueljenni <manuelnoahjenni@outlook.com>
2024-10-18 11:00:30 +0700

    Placeholder

--- components/custom/questionnaire/TagespackPlaceholder.tsx
+++ components/custom/questionnaire/TagespackPlaceholder.tsx
@@ -1,4 +1,5 @@
 import { AnswerType, Question, QuestionId } from "@/app/questionnaire/types";
+import tagespack from "@/public/images/tagespack-energie.png";
 import Image from "next/image";
 import QuestionContainer from "./Question";
 
@@ -7,19 +8,23 @@ export default function TagespackPlaceholder(props: {
   onAnswer: <T extends QuestionId>(questionId: T, answers: AnswerType<T>[]) => void;
 }) {
   return (
-    <div className="relative w-full h-full">
-      <QuestionContainer
-        question={props.question}
-        showSubmitButton
-        onSubmit={() => props.onAnswer(props.question.id, [])}
-      />
-      <div className="absolute h-screen w-full">
-        <Image
-          src="/images/landing/tagespack.png"
-          alt="Tagespack"
-          width={200}
-          height={200}
-          className="absolute inset-0 object-contain mx-auto h-1/2 w-1/2"
+    <div className="relative w-full h-screen">
+      <div className="absolute inset-0 z-10">
+        <div className="relative w-full h-[150%] mx-auto">
+          <Image
+            src={tagespack}
+            alt="Tagespack"
+            fill
+            className="object-contain object-center"
+            priority
+          />
+        </div>
+      </div>
+      <div className="absolute inset-0 z-20 flex items-start justify-center pt-8">
+        <QuestionContainer
+          question={props.question}
+          showSubmitButton
+          onSubmit={() => props.onAnswer(props.question.id, [])}
         />
       </div>
     </div>
