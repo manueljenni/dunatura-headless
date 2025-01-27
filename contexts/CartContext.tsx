40ae22d8d7f7659645e8ac73bd6e18e7523a2c64
manueljenni <manuelnoahjenni@gmail.com>
2025-01-27 17:27:43 +0000

    FIx types

--- contexts/CartContext.tsx
+++ contexts/CartContext.tsx
@@ -68,7 +68,7 @@ export function CartProvider({ children }: { children: React.ReactNode }) {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          cartId: typeof cartId === "object" ? cartId.toString() : cartId,
+          cartId: cartId,
           variantId,
         }),
       });
@@ -123,7 +123,7 @@ export function CartProvider({ children }: { children: React.ReactNode }) {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          cartId: typeof cartId === "object" ? cartId.toString() : cartId,
+          cartId: cartId,
         }),
       });
 
